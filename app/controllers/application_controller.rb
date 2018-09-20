473c34d3754c282b28af92cdfd450bcf02ba2fbe
Cory Tveten <corytveten@gmail.com>
2018-09-20 12:02:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,11 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
   patch '/posts/:id' do
     @post = Post.find(params[:id])
     @post.name = params[:name]
